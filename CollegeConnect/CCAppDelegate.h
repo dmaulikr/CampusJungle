afffeed93161befabb616de5bc068c42824210dd
Julia Petryshena <y.petryshena@111minutes.com>
2013-05-08 16:10:54 +0300

    Initial Commit

---
+++ CollegeConnect/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  CollegeConnect
+//
+//  Created by Yulia Petryshena on 5/8/13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollegeConnect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollegeConnect
+//
+//  Created by Yulia Petryshena on 5/8/13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
