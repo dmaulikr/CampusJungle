0b75237be633040a606f673e82e55d6ac99d942f
korzun1993 <korzun1993@mail.ru>
2013-05-22 19:05:27 +0300

    added untreced files

---
+++ CollegeConnect/Controllers/WelcomeScreen/CCWelcomController.m
@@ -0,0 +1,50 @@
+//
+//  CCLoginController.m
+//  CollegeConnect
+//
+//  Created by Vlad Korzun on 21.05.13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import "CCWelcomeController.h"
+#import "CCLoginAPIProviderProtocol.h"
+
+@interface CCWelcomeController ()
+
+@property (nonatomic, strong) id <CCLoginAPIProviderProtocol> ioc_loginAPIProvider;
+
+@end
+
+@implementation CCWelcomeController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (IBAction)facebookLoginButtonDidPressed
+{
+    [self.ioc_loginAPIProvider performLoginOperationViaFacebookWithSuccessHandler:^{
+        
+    } errorHandler:^(NSError * error) {
+        
+    }];
+}
+
+- (IBAction)twitterLoginButtonDidPressed
+{
+
+}
+
+- (IBAction)emailLoginButtonDidPressed
+{
+
+}
+
+- (IBAction)emailSignUPButtonDidPressed
+{
+    [self.signUpTransaction perform];
+}
+
+@end
