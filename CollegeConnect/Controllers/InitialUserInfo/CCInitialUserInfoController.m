46a21a0e9f1cef9f3361fc6d146bbffbaf39ed8f
korzun1993 <korzun1993@mail.ru>
2013-05-23 19:57:22 +0300

    removed backbutton fron initial user info screen

--- CollegeConnect/Controllers/InitialUserInfo/CCInitialUserInfoController.m
+++ CollegeConnect/Controllers/InitialUserInfo/CCInitialUserInfoController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.navigationItem.leftBarButtonItem = nil;
+    self.navigationItem.hidesBackButton = YES;
 }
 
 - (IBAction)continueButtonDidPressed
