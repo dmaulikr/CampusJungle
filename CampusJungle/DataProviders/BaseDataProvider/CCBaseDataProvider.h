0bd3f546f0bdc11e72b1468725f62c2270c3f148
Julia Petryshena <y.petryshena@111minutes.com>
2013-06-03 18:20:34 +0300

    Class model added

--- CampusJungle/DataProviders/BaseDataProvider/CCBaseDataProvider.h
+++ CampusJungle/DataProviders/BaseDataProvider/CCBaseDataProvider.h
@@ -26,5 +26,4 @@
 - (void)showErrorWhileLoading:(NSError *)error;
 - (BOOL)isEmpty;
 
-
 @end
---
+++ CampusJungle/Domain/Models/Class/CCClass.h
@@ -0,0 +1,20 @@
+//
+//  CCClass.h
+//  CampusJungle
+//
+//  Created by Yulia Petryshena on 6/3/13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCClass : NSObject
+
+@property (nonatomic, strong) NSString *professor;
+@property (nonatomic, strong) NSString *collegeID;
+@property (nonatomic, strong) NSString *semester;
+@property (nonatomic, strong) NSString *subject;
+@property (nonatomic, strong) NSArray *timetable;
+@property (nonatomic, strong) NSDictionary *timeClassesAreTaken;
+
+@end
