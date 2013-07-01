728e47e457dba9c2b8b529d7a35b6d8b0cf9aa1a
korzun1993 <korzun1993@mail.ru>
2013-07-01 16:52:49 +0300

    giment controller customization

--- CampusJungle/Services/AppearanceConfigurator/CCAppearanceConfigurator.m
+++ CampusJungle/Services/AppearanceConfigurator/CCAppearanceConfigurator.m
@@ -12,22 +12,46 @@
 
 + (void)configurate
 {
+    [CCAppearanceConfigurator configurateTextFields];
+    [CCAppearanceConfigurator configurateNavigationBar];
+    [CCAppearanceConfigurator configurateBarButtons];
+    [CCAppearanceConfigurator configurateButton];
+    [CCAppearanceConfigurator configurateSegmentController];
+}
+
++ (void)configurateTextFields
+{
+    [[UISearchBar appearance] setTintColor:[UIColor brownColor]];
+    
+    [[UITextField appearance] setBackground:[[UIImage imageNamed:@"input_field"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)]];
+    [[UITextField appearance] setBorderStyle:UITextBorderStyleNone];
+
+}
+
++ (void)configurateNavigationBar
+{
     UIImage *navBackgroundImage = [[UIImage imageNamed:@"navigation_bar"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
     [[UINavigationBar appearance] setBackgroundImage:navBackgroundImage forBarMetrics:UIBarMetricsDefault];
     
     [[UINavigationBar appearance] setTitleTextAttributes:@{
                                 UITextAttributeTextColor: [UIColor colorWithRed:240./255 green:218./255 blue:161./255 alpha:1]
-                                  }];
-    
+     }];
+}
+
++ (void)configurateBarButtons
+{
     UIImage *backButtonImage = [[UIImage imageNamed:@"back_button"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 13, 0, 6)];
     [[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
     UIImage *barButtonImage = [[UIImage imageNamed:@"action_button"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 6, 0, 6)];
     [[UIBarButtonItem appearance] setBackgroundImage:barButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
     
     [[UIBarButtonItem appearance] setTitleTextAttributes:@{
-       UITextAttributeTextColor : [UIColor colorWithRed:240./255 green:218./255 blue:161./255 alpha:1]
+                               UITextAttributeTextColor : [UIColor colorWithRed:240./255 green:218./255 blue:161./255 alpha:1]
      } forState:UIControlStateNormal];
-    
+}
+
++ (void)configurateButton
+{
     UIImage *customButtonBackground = [[UIImage imageNamed:@"button"] resizableImageWithCapInsets:UIEdgeInsetsMake(20, 10, 20, 10)];
     
     UIImage *customButtonActiveBackground = [UIImage imageNamed:@"button_active"];
@@ -39,13 +63,21 @@
     [[UILabel appearanceWhenContainedIn:[UIButton class], nil] setFont:[UIFont fontWithName:@"HelveticaNeue-CondensedBold" size:17]];
     
     [[UILabel appearanceWhenContainedIn:[UIButton class], nil] setTextColor:[UIColor yellowColor]];
+}
+
++ (void)configurateSegmentController
+{
+    [[UISegmentedControl appearance] setBackgroundImage:[UIImage imageNamed:@"button"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [[UISegmentedControl appearance] setBackgroundImage:[UIImage imageNamed:@"button_active"] forState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
+    [[UISegmentedControl appearance] setDividerImage:[[UIImage imageNamed:@"separator"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)]
+                                 forLeftSegmentState:UIControlStateNormal
+                                   rightSegmentState:UIControlStateSelected
+                                          barMetrics:UIBarMetricsDefault];
     
-    [[UISearchBar appearance] setTintColor:[UIColor brownColor]];
-    
-    [[UITextField appearance] setBackground:[[UIImage imageNamed:@"input_field"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)]];
-    [[UITextField appearance] setBorderStyle:UITextBorderStyleNone];
-    
-    [[UITableViewCell appearance] setAccessoryType:UITableViewCellAccessoryNone];
+    [[UISegmentedControl appearance] setTitleTextAttributes:@{
+                                  UITextAttributeTextColor : [UIColor colorWithRed:240./255 green:218./255 blue:161./255 alpha:1],
+                            UITextAttributeTextShadowColor : [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.0],
+     } forState:UIControlStateNormal];
 }
 
 @end
