6281dd3a5dea0327672abe7b2bae9b1c96c88a05
korzun1993 <korzun1993@mail.ru>
2013-07-10 19:06:10 +0300

    rate in process

--- CampusJungle/Controllers/RateController/CCRateController.m
+++ CampusJungle/Controllers/RateController/CCRateController.m
@@ -8,10 +8,16 @@
 
 #import "CCRateController.h"
 #import "DYRateView.h"
+#import "CCStandardErrorHandler.h"
 
 @interface CCRateController ()
 
 @property (nonatomic, strong) DYRateView *rateView;
+@property (nonatomic, weak) IBOutlet UIImageView *textFieldBackground;
+@property (nonatomic, weak) IBOutlet UITextView *textFiled;
+@property (nonatomic, weak) IBOutlet UIView *rateViewContainer;
+
+- (IBAction)rate;
 
 @end
 
@@ -20,11 +26,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.rateView = [[DYRateView alloc] initWithFrame:CGRectMake(0, 0, 320, 100)];
-    [self.view addSubview:self.rateView];
+    [self configStars];
+    self.textFieldBackground.image = [[UIImage imageNamed:@"text_box"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    self.tapRecognizer.enabled = YES;
+    self.title = @"Rate";
+    [(TPKeyboardAvoidingTableView *)self.view setScrollEnabled:NO];
+}
+
+- (void)configStars
+{
+    self.rateView = [[DYRateView alloc] initWithFrame:CGRectMake(0, 0, 320, 100) fullStar:[UIImage imageNamed:@"StarFullLarge"] emptyStar:[UIImage imageNamed:@"StarEmptyLarge"]];
+    self.rateView.rate = 5;
+    [self.rateViewContainer addSubview:self.rateView];
     self.rateView.editable = YES;
     self.rateView.alignment = RateViewAlignmentCenter;
+}
+
+- (IBAction)rate
+{
+    if(self.textFiled.text.length){
+//        [self.ioc_messageAPIProvider sendMessage:self.inputField.text
+//                                          toUser:self.recipient.uid
+//                                  successHandler:^(id result) {
+//                                      [self.successMessageSendTransaction perform];
+//                                  }
+//                                    errorHandler:^(NSError *error) {
+//                                        [CCStandardErrorHandler showErrorWithError:error];
+//                                    }];
+    } else {
+        [CCStandardErrorHandler showErrorWithTitle:nil message:@"Rate can not be without comment"];
+    }
+}
+
+- (BOOL)textView:(UITextView *)txtView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    if( [text rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].location == NSNotFound ) {
+        return YES;
+    }
     
+    [self rate];
+    return NO;
 }
 
 @end
