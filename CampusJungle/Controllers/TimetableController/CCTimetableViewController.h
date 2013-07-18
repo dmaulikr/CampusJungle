239c2754097d75c5117923bf0f3f03173b24a829
Yury Grinenko <y.grinenko@111minutes.com>
2013-07-18 13:18:59 +0300

    added timetable controller stub

---
+++ CampusJungle/Controllers/TimetableController/CCTimetableViewController.h
@@ -0,0 +1,14 @@
+//
+//  CCTimetableViewController.h
+//  CampusJungle
+//
+//  Created by Yury Grinenko on 18.07.13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CCBaseViewController.h"
+
+@interface CCTimetableViewController : CCBaseViewController
+
+@end
---
+++ CampusJungle/Controllers/TimetableController/CCTimetableViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCTimetableViewController.m
+//  CampusJungle
+//
+//  Created by Yury Grinenko on 18.07.13.
+//  Copyright (c) 2013 111minutes. All rights reserved.
+//
+
+#import "CCTimetableViewController.h"
+
+@interface CCTimetableViewController ()
+
+@end
+
+@implementation CCTimetableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
